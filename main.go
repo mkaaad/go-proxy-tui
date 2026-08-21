d2813761b3c23e2278c95f339c229119b337c3e9
mkaaad <kadmanmk@outlook.com>
2026-08-21 10:47:32 +0800

    all: init commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/rivo/tview"
+)
+
+func main() {
+	box := tview.NewBox().
+		SetBorder(true).
+		SetTitle("hello world")
+
+	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
+		panic(err)
+	}
+}
